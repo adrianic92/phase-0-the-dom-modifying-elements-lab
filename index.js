fc6e25b2a0804127894547736d3616015369c907
Adrian <adrianic92@yahoo.com>
2022-07-25 09:05:40 -0700

    commit tree 951d0b15998c64b27851a55f09071556c71d8c56

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainID = document.getElementById("main");
+mainID.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Adrian is the champion";
+newHeader.className = "victory";
